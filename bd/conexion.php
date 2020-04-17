5e605bab05d72e833c6a9c6a1d14f021527e575c
Miguel Villalba <54925140+RiderMike27@users.noreply.github.com>
2020-04-16 22:05:53 -0400

    Update conexion.php
    update

--- bd/conexion.php
+++ bd/conexion.php
@@ -4,8 +4,7 @@
             define('servidor', 'localhost');
             define('nombre_bd', 'crudconvuejs');
             define('usuario', 'postgres');
-            define('password', 'dgtic123');	
-            //$opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
+            define('password', 'dgtic123');
             try{
                 $conexion = new PDO("pgsql:host=".servidor."; dbname=".nombre_bd, usuario, password);
                 return $conexion;
